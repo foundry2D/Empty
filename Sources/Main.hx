e0ea15945602cb88922b3ce28e963fba39c42d03
m9-unix <mundusnine@gmail.com>
2020-02-06 15:48:17 -0500

    Update Main.hx

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,7 +4,7 @@ import found.Found;
 
 class Main {
 	public static function main(){
-		//To see the whole FoundConfig go have a look at of Found class in Foundry2d
+		//To see the whole FoundConfig, go have a look at Found.hx in foundry2d library
 		Found.setup({app:Project, title:'MyProjectName'});
 	}
 }

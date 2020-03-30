e24e32f842780958c00aac1da1333329d0757210
jsn213 <jean-sebastien.nadeau.3@ulaval.ca>
2020-03-30 18:19:53 -0400

    Push missing stuff

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -3,8 +3,13 @@ package;
 import found.Found;
 
 class Main {
+
+	public static var version = "0.1";
+	public static var sha = utilities.BuildMacros.sha().substr(1, 7);
+	public static var date = utilities.BuildMacros.date().split(" ")[0];
+
 	public static function main(){
 		//To see the whole FoundConfig, go have a look at Found.hx in foundry2d library
-		Found.setup({app:Project, title:'MyProjectName'});
+		Found.setup({app:Project, title:'Foundry2d'});
 	}
 }

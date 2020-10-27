38bd56180e7ef53bdfe1663ff247561b94f24a16
mundusnine <mundusnine@gmail.com>
2020-10-27 17:06:10 -0400

    Removed unneeded in individual projects sha and date

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,9 +4,6 @@ import found.Found;
 
 class Main {
 
-	public static var sha = utilities.BuildMacros.sha().substr(1, 7);
-	public static var date = utilities.BuildMacros.date().split(" ")[0];
-
 	public static function main(){
 		//To see the whole FoundConfig, go have a look at Found.hx in foundry2d library
 		Found.setup({app:Project, title:'Foundry2d'});
